904e4c5d0f1942878d6e04fe44a397a997bc8aa6
Michael McLoughlin <mmcloughlin@gmail.com>
2015-10-12 19:04:45 -0700

    start of a CLI interface

--- main.go
+++ main.go
@@ -6,16 +6,59 @@ import (
 	"net/url"
 
 	"github.com/google/go-github/github"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	// Create DB
-	store, err := NewStore("./store.db")
-	if err != nil {
-		log.Fatal(err)
+	// Main command
+	var storePath string
+	var store *Store
+	mainCmd := &cobra.Command{
+		Use:   "ghr",
+		Short: "Scrape GitHub for tech talent",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			s, err := NewStore("./store.db")
+			if err != nil {
+				log.Fatal(err)
+			}
+			store = s
+		},
 	}
-	store.Init()
 
+	mainCmd.PersistentFlags().StringVarP(&storePath, "store", "", "",
+		"Path to datastore (SQLite3 database).")
+
+	// Initialize datastore
+	initCmd := &cobra.Command{
+		Use:   "init",
+		Short: "Initialize datastore",
+		Run: func(cmd *cobra.Command, args []string) {
+			store.Init()
+		},
+	}
+
+	mainCmd.AddCommand(initCmd)
+
+	// Start a query
+	var searchQuery string
+	searchCmd := &cobra.Command{
+		Use:   "search --query=QUERY",
+		Short: "Start a new query",
+		Run: func(cmd *cobra.Command, args []string) {
+			Query(store, searchQuery)
+		},
+	}
+
+	searchCmd.PersistentFlags().StringVarP(&searchQuery, "query", "", "",
+		"Search keywords and qualifiers (https://developer.github.com/v3/search/#search-repositories)")
+
+	mainCmd.AddCommand(searchCmd)
+
+	// Execute
+	_ = mainCmd.Execute()
+}
+
+func Query(store *Store, q string) {
 	// proxy layer
 	u, _ := url.Parse("http://localhost:11112")
 	proxyTransport := &http.Transport{
@@ -40,7 +83,7 @@ func main() {
 	}
 
 	// Start scrape
-	s, err := store.NewSearch("stars:>=1000 language:Go")
+	s, err := store.NewSearch(q)
 	if err != nil {
 		log.Fatal(err)
 	}
